93a83dccae2f5b7ae99f70551679fd3c409e3960
tzzs <1789803837@qq.com>
2019-06-25 02:34:05 +0800

    更新数据读入

--- Main.py
+++ Main.py
@@ -2,20 +2,31 @@ import tensorflow as tf
 from tensorflow import keras
 
 import numpy as np
+import nltk
+import nltk.stem
+import re
+from nltk.tokenize import word_tokenize
+import pandas
+import csv
 
 label = []
 message = []
 
-with open('train.csv', encoding='utf-8') as f:
-    data = f.readlines()[1:]
+with open('train.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    rows = [row[:2] for row in reader]
 
-print(data)
+print(rows)
 
-for d in data:
-    d = d.split(',', 1)
-
-    label.append(d[0])
-    message.append(d[1])
+for row in rows[1:]:
+    if row[0] == 'ham':
+        label.append(0)
+    elif row[0] == 'spam':
+        label.append(1)
+    else:
+        label.append(-1)
+        print(row)
+    message.append(row[1].lower())
 
 print(label)
 print(message)
