05dd3afc707caf6e49e70cfb570945bb677df952
tzzs <1789803837@qq.com>
2019-06-25 02:49:11 +0800

    生成词表

--- Main.py
+++ Main.py
@@ -1,13 +1,6 @@
-import tensorflow as tf
-from tensorflow import keras
-
-import numpy as np
-import nltk
-import nltk.stem
-import re
-from nltk.tokenize import word_tokenize
-import pandas
 import csv
+import re
+from itertools import chain
 
 label = []
 message = []
@@ -16,7 +9,7 @@ with open('train.csv', 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
     rows = [row[:2] for row in reader]
 
-print(rows)
+# print(rows)
 
 for row in rows[1:]:
     if row[0] == 'ham':  # 非垃圾短信
@@ -29,5 +22,11 @@ for row in rows[1:]:
     text = re.sub(r'[^a-z ]', '', row[1].lower())
     message.append(text)
 
-print(label)
-print(message)
+# print('label\n', label)
+# print('message\n', message)
+
+docs = [m.split() for m in message]
+
+# 生成词表
+words = list(set(chain(*docs)))  # 加*是将列表拆分为两个参数
+print(words)
